c1e083fb09266550a014e1b33b74b07c9c9e1e2c
Ivan Pilot <ivanpilot@gmail.com>
2022-06-07 12:15:22 +0200

    added 404

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+    text-align: center;
+    font-size: 6rem;
+    padding: 2rem;
+`;
+
+export default function Custom404() {
+    return <H1>404 - Page Not Found</H1>;
+}
